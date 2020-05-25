d74cb614430c7c4ee40927cc6cb5e3360e6cca33
PopovDaniil <44977912+PopovDaniil@users.noreply.github.com>
2020-05-25 19:06:09 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const querystring = require("querystring");
+const express = require("express");
+const expressHbs = require("express-handlebars");
+const hbs = require("hbs");
+const app = express();
+  
+// устанавливаем настройки для файлов layout
+app.engine("hbs", expressHbs(
+    {
+        layoutsDir: "views/layouts", 
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+))
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+hbs.registerPartials(__dirname + "/views/partials");
+ 
+app.get("/catalog/*", (req,res) => {
+    console.log(path.basename(req.url))
+})
+
+app.get("/", (req,res) => {
+    res.render("home.hbs");
+});
+
+app.get("/*", (req, res) => {
+    let file = path.basename(req.url);
+    res.render(`${file}.hbs`);
+})
+app.listen(3000);
